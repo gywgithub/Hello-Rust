5970626398c70a9d1ca26164c6176c3fc06d5875
gywgithub <qingyi_w@outlook.com>
2024-01-30 13:58:34 +0800

    update main.rs

--- data-type-number/src/main.rs
+++ data-type-number/src/main.rs
@@ -57,6 +57,88 @@ fn main() {
 
     println!("{}", (2.0_f64).sqrt());
     println!("{}", f64::sqrt(2.0));
+
+    // --- 3.2 布尔类型 ---
+    assert_eq!(false as i32, 0);
+    assert_eq!(true as i32, 1);
+
+    // --- 3.4 元组 ---
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+
+    // --- 3.6 数组、向量和切片 ---
+    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+
+    assert_eq!(lazy_caterer[3], 7);
+    assert_eq!(taxonomy.len(), 3);
+
+    let mut sieve = [true; 10000];
+    for i in 2..100 {
+        if sieve[i] {
+            let mut j = i * i;
+            while j < 10000 {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    assert!(sieve[211]);
+    assert!(!sieve[9876]);
+
+    let mut chaos = [3, 5, 4, 1, 2];
+    chaos.sort();
+    assert_eq!(chaos, [1, 2, 3, 4, 5]);
+
+    let mut primes = vec![2, 3, 5, 7];
+    assert_eq!(primes.iter().product::<i32>(), 210);
+
+    primes.push(11);
+    primes.push(13);
+    assert_eq!(primes.iter().product::<i32>(), 30030);
+
+    // fn new_pixel_buffer(rows: usize, cols: usize) -> Vec<u8> {
+    //     vec![0; rows * cols]
+    // }
+
+    let mut pal = Vec::new();
+    pal.push("step");
+    pal.push("on");
+    pal.push("no");
+    pal.push("pets");
+    assert_eq!(pal, vec!["step", "on", "no", "pets"]);
+
+    let v: Vec<i32> = (0..5).collect();
+    assert_eq!(v, [0, 1, 2, 3, 4]);
+
+    let mut v = vec![10, 20, 30, 40, 50];
+    v.insert(3, 35);
+    assert_eq!(v, [10, 20, 30, 35, 40, 50]);
+
+    v.remove(1);
+    assert_eq!(v, [10, 30, 35, 40, 50]);
+
+    let mut v = vec!["Snow Puff", "Glass Gem"];
+    assert_eq!(v.pop(), Some("Glass Gem"));
+    assert_eq!(v.pop(), Some("Snow Puff"));
+    assert_eq!(v.pop(), None);
+
+    let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+    let a: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
+
+    let sv: &[f64] = &v;
+    let sa: &[f64] = &a;
+
+    fn print(n: &[f64]) {
+        for elt in n {
+            println!("{}", elt);
+        }
+    }
+
+    print(&a);
+    print(&v);
 }
 
 // fn build_vector() -> Vec<i16> {
