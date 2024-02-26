e70a785facaedf1168a139881b568553cba1dc8a
gywgithub <qingyi_w@outlook.com>
2024-02-26 15:50:39 +0800

    update main.rs

--- data-type-number/src/main.rs
+++ data-type-number/src/main.rs
@@ -1,3 +1,5 @@
+// use std::rc::Rc;
+
 fn main() {
     println!("Hello, world!");
 
@@ -32,113 +34,200 @@ fn main() {
 
     // assert_eq!(5_i16.wrapping_shl(17), 10);
 
-    // 饱和运算 saturating_add 饱和整数加法
-    // saturating v.使饱和，浸透，使充满
-    assert_eq!(32760_i16.saturating_add(10), 32767);
+    // // 饱和运算 saturating_add 饱和整数加法
+    // // saturating v.使饱和，浸透，使充满
+    // assert_eq!(32760_i16.saturating_add(10), 32767);
 
-    assert_eq!((-32760_i16).saturating_sub(10), -32768);
+    // assert_eq!((-32760_i16).saturating_sub(10), -32768);
 
-    // 溢出运算 返回元组(result, overflowd), overflowd 表示是否溢出
-    // sub 减法
-    // assert_eq!(255_u8.overflowing_sub(2), (253, false));
-    // assert_eq!(255_u8.overflowing_add(3), (2, true));
+    // // 溢出运算 返回元组(result, overflowd), overflowd 表示是否溢出
+    // // sub 减法
+    // // assert_eq!(255_u8.overflowing_sub(2), (253, false));
+    // // assert_eq!(255_u8.overflowing_add(3), (2, true));
 
-    // assert_eq!(5_u16.overflowing_shl(17), (10, true));
+    // // assert_eq!(5_u16.overflowing_shl(17), (10, true));
 
-    // --- 3.1.3 浮点类型 ---
-    // assert!((-1. / f32::INFINITY).is_sign_negative());
+    // // --- 3.1.3 浮点类型 ---
+    // // assert!((-1. / f32::INFINITY).is_sign_negative());
 
-    // assert_eq!(-f32::MIN, f32::MAX)
+    // // assert_eq!(-f32::MIN, f32::MAX)
 
-    // sqrt() 平方根
-    // square root 平方根
-    // assert_eq!(5f32.sqrt() * 5f32.sqrt(), 5.);
-    // assert_eq!((-1.01f64).floor(), -2.0);
+    // // sqrt() 平方根
+    // // square root 平方根
+    // // assert_eq!(5f32.sqrt() * 5f32.sqrt(), 5.);
+    // // assert_eq!((-1.01f64).floor(), -2.0);
 
-    println!("{}", (2.0_f64).sqrt());
-    println!("{}", f64::sqrt(2.0));
+    // println!("{}", (2.0_f64).sqrt());
+    // println!("{}", f64::sqrt(2.0));
 
-    // --- 3.2 布尔类型 ---
-    assert_eq!(false as i32, 0);
-    assert_eq!(true as i32, 1);
+    // // --- 3.2 布尔类型 ---
+    // assert_eq!(false as i32, 0);
+    // assert_eq!(true as i32, 1);
 
-    // --- 3.4 元组 ---
-    let text = "I see the eigenvalue in thine eye";
-    let (head, tail) = text.split_at(21);
-    assert_eq!(head, "I see the eigenvalue ");
-    assert_eq!(tail, "in thine eye");
+    // // --- 3.4 元组 ---
+    // let text = "I see the eigenvalue in thine eye";
+    // let (head, tail) = text.split_at(21);
+    // assert_eq!(head, "I see the eigenvalue ");
+    // assert_eq!(tail, "in thine eye");
 
-    // --- 3.6 数组、向量和切片 ---
-    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
-    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+    // // --- 3.6 数组、向量和切片 ---
+    // let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    // let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
 
-    assert_eq!(lazy_caterer[3], 7);
-    assert_eq!(taxonomy.len(), 3);
+    // assert_eq!(lazy_caterer[3], 7);
+    // assert_eq!(taxonomy.len(), 3);
 
-    let mut sieve = [true; 10000];
-    for i in 2..100 {
-        if sieve[i] {
-            let mut j = i * i;
-            while j < 10000 {
-                sieve[j] = false;
-                j += i;
-            }
-        }
-    }
-    assert!(sieve[211]);
-    assert!(!sieve[9876]);
+    // let mut sieve = [true; 10000];
+    // for i in 2..100 {
+    //     if sieve[i] {
+    //         let mut j = i * i;
+    //         while j < 10000 {
+    //             sieve[j] = false;
+    //             j += i;
+    //         }
+    //     }
+    // }
+    // assert!(sieve[211]);
+    // assert!(!sieve[9876]);
 
-    let mut chaos = [3, 5, 4, 1, 2];
-    chaos.sort();
-    assert_eq!(chaos, [1, 2, 3, 4, 5]);
+    // let mut chaos = [3, 5, 4, 1, 2];
+    // chaos.sort();
+    // assert_eq!(chaos, [1, 2, 3, 4, 5]);
 
-    let mut primes = vec![2, 3, 5, 7];
-    assert_eq!(primes.iter().product::<i32>(), 210);
+    // let mut primes = vec![2, 3, 5, 7];
+    // assert_eq!(primes.iter().product::<i32>(), 210);
 
-    primes.push(11);
-    primes.push(13);
-    assert_eq!(primes.iter().product::<i32>(), 30030);
+    // primes.push(11);
+    // primes.push(13);
+    // assert_eq!(primes.iter().product::<i32>(), 30030);
 
-    // fn new_pixel_buffer(rows: usize, cols: usize) -> Vec<u8> {
-    //     vec![0; rows * cols]
+    // // fn new_pixel_buffer(rows: usize, cols: usize) -> Vec<u8> {
+    // //     vec![0; rows * cols]
+    // // }
+
+    // let mut pal = Vec::new();
+    // pal.push("step");
+    // pal.push("on");
+    // pal.push("no");
+    // pal.push("pets");
+    // assert_eq!(pal, vec!["step", "on", "no", "pets"]);
+
+    // let v: Vec<i32> = (0..5).collect();
+    // assert_eq!(v, [0, 1, 2, 3, 4]);
+
+    // let mut v = vec![10, 20, 30, 40, 50];
+    // v.insert(3, 35);
+    // assert_eq!(v, [10, 20, 30, 35, 40, 50]);
+
+    // v.remove(1);
+    // assert_eq!(v, [10, 30, 35, 40, 50]);
+
+    // let mut v = vec!["Snow Puff", "Glass Gem"];
+    // assert_eq!(v.pop(), Some("Glass Gem"));
+    // assert_eq!(v.pop(), Some("Snow Puff"));
+    // assert_eq!(v.pop(), None);
+
+    // let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+    // let a: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
+
+    // let sv: &[f64] = &v;
+    // let sa: &[f64] = &a;
+
+    // fn print(n: &[f64]) {
+    //     for elt in n {
+    //         println!("{}", elt);
+    //     }
     // }
 
-    let mut pal = Vec::new();
-    pal.push("step");
-    pal.push("on");
-    pal.push("no");
-    pal.push("pets");
-    assert_eq!(pal, vec!["step", "on", "no", "pets"]);
+    // print(&a);
+    // print(&v);
 
-    let v: Vec<i32> = (0..5).collect();
-    assert_eq!(v, [0, 1, 2, 3, 4]);
+    // // --- 3.7 字符串类型 ---
+    // let method = b"GET";
+    // assert_eq!(method, &[b'G', b'E', b'T']);
 
-    let mut v = vec![10, 20, 30, 40, 50];
-    v.insert(3, 35);
-    assert_eq!(v, [10, 20, 30, 35, 40, 50]);
+    // let noodles = "noodles".to_string();
+    // let oodless = &noodles[1..];
+    // let poodless = "ಠ_ಠ";
 
-    v.remove(1);
-    assert_eq!(v, [10, 30, 35, 40, 50]);
+    // println!("{}", noodles);
 
-    let mut v = vec!["Snow Puff", "Glass Gem"];
-    assert_eq!(v.pop(), Some("Glass Gem"));
-    assert_eq!(v.pop(), Some("Snow Puff"));
-    assert_eq!(v.pop(), None);
+    // // --- 4 所有权与移动 ---
+    // struct Person {
+    //     name: String,
+    //     birth: i32,
+    // }
 
-    let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
-    let a: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
+    // let mut composers = Vec::new();
+    // composers.push(Person {
+    //     name: "Palestrina".to_string(),
+    //     birth: 1525,
+    // });
+    // composers.push(Person {
+    //     name: "Downland".to_string(),
+    //     birth: 1563,
+    // });
+    // composers.push(Person {
+    //     name: "Lully".to_string(),
+    //     birth: 1632,
+    // });
 
-    let sv: &[f64] = &v;
-    let sa: &[f64] = &a;
+    // for composer in &composers {
+    //     println!("{}, born {}", composer.name, composer.birth);
+    // }
 
-    fn print(n: &[f64]) {
-        for elt in n {
-            println!("{}", elt);
-        }
-    }
+    // let s: Rc<String> = Rc::new("shirataki".to_string());
+    // let t: Rc<String> = s.clone();
+    // let u: Rc<String> = s.clone();
 
-    print(&a);
-    print(&v);
+    // struct Person { name: String, birth: i32 }
+
+    // let mut composers = Vec::new();
+
+    // composers.push(Person { name: "Palestrina".to_string(), birth: 1525 });
+    // composers.push(Person { name: "Download".to_string(), birth: 1563 });
+    // composers.push(Person { name: "Lully".to_string(), birth: 1632 });
+
+    // for composer in &composers {
+    //     println!("{}, born {}", composer.name, composer.birth);
+    // }
+
+    // --- 4.2.1 移动 ---
+    // let mut s = "Govinda".to_string();
+    // let t = s;
+    // // println!("{}", s);
+
+    // println!("{}", t);
+    // s = "Siddhartha".to_string();
+    // println!("{}", s);
+
+    // let mut v = Vec::new();
+    // for i in 101 .. 106 {
+    //     v.push(i.to_string());
+    // }
+
+    // let third = &v[2];
+    // println!("{}", third);
+
+    // struct Person { name: Option<String>, birth: i32 }
+
+    // let mut composers = Vec::new();
+    // composers.push(Person { name: Some("Palestrina".to_string()), birth: 1525 });
+
+    // let first_name = std::mem::replace(&mut composers[0].name, None);
+    // assert_eq!(first_name, Some("Palestrina".to_string()));
+    // assert_eq!(composers[0].name, None);
+
+    #[derive(Copy, Clone)]
+    struct Label { number: u32 }
+
+    fn print(l: Label) { println!("STAMP: {}", l.number); }
+
+    let l = Label { number: 3 };
+
+    print(l);
+
+    println!("My label number is: {}", l.number);
 }
 
 // fn build_vector() -> Vec<i16> {
