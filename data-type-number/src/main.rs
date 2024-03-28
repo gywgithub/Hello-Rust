6e3b528a8d5d1b30bc0c6f88095692ba90c2c665
gywgithub <qingyi_w@outlook.com>
2024-03-28 16:37:59 +0800

    add exercise code

--- data-type-number/src/main.rs
+++ data-type-number/src/main.rs
@@ -17,7 +17,6 @@
 // type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
 // type GenericResult<T> = Reulst<T, GenericError>;
 
-
 // fn main() -> Result<(), TideCalcError> {
 //     let tides = calculate_tides()?;
 //     print_tides(tides);
@@ -30,8 +29,179 @@
 //     pub column: usize,
 // }
 
+// #[test]
+// fn math_works() {
+//     let x: i32 = 1;
+//     assert!(x.is_positive());
+//     assert_eq!(x + 1, 2);
+// }
+
+// #[test]
+// #[allow(unconditional_panic, unused_must_use)]
+// #[should_panic(expected = "divide by zero")]
+// fn test_divide_by_zero_error() {
+//     1 / 0;
+// }
+
+// use std::num::ParseIntError;
+
+// #[test]
+// fn explicit_radix() -> Result<(), ParseIntError> {
+//     i32::from_str_radix("1024", 10)?;
+//     Ok(())
+// }
+
+// fn roughly_equal(a: f64, b: f64) -> bool {
+//     (a - b).abs() < 1e-6
+// }
+
+// #[test]
+// fn trig_works() {
+//     use std::f64::consts::PI;
+//     assert!(roughly_equal(PI.sin(), 0.0))
+// }
+
+// struct GrayscaleMap {
+//     pixels: Vec<u8>,
+//     size: (usize, usize)
+// }
+
+// let width = 1024;
+// let height = 576;
+// let image = GrayscaleMap {
+//     pixels: vec![0; width * height],
+//     size: (width, height)
+// }
+
+// fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
+//     assert_eq!(pixels.len(), size.0 * size.1);
+//     GrayscaleMap { pixels, size }
+// }
+
+// assert_eq!(image.size, (1024, 576));
+// assert_eq!(image.pixels.len(), 1024 * 576);
+
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut q = Queue {
+        older: Vec::new(),
+        younger: Vec::new(),
+    };
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    let (older, younger) = q.split();
+
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
+
+    // q.push('0');
+    // q.push('1');
+    // assert_eq!(q.pop(), Some('0'));
+
+    // q.push('∞');
+    // assert_eq!(q.pop(), Some('1'));
+    // assert_eq!(q.pop(), Some('∞'));
+    // assert_eq!(q.pop(), None);
+
+    // assert!(q.is_empty());
+    // q.push('⊙');
+    // assert!(!q.is_empty())
+
+    // struct Bounds(usize, usize);
+
+    // let image_bounds = Bounds(1024, 768);
+
+    // assert_eq!(image_bounds.0 * image_bounds.1, 786432);
+
+    // fn Bounds(elem0: usize, elem1: usize) -> Bounds { ... }
+
+    // ---
+    // struct Broom {
+    //     name: String,
+    //     height: u32,
+    //     health: u32,
+    //     position: (f32, f32, f32),
+    //     intent: BroomIntent,
+    // }
+
+    // #[derive(Copy, Clone)]
+    // enum BroomIntent {
+    //     FetchWater,
+    //     DumpWater,
+    // }
+
+    // fn chop(b: Broom) -> (Broom, Broom) {
+    //     let mut broom1 = Broom {
+    //         height: b.height / 2,
+    //         ..b
+    //     };
+
+    //     let mut broom2 = Broom {
+    //         name: broom1.name.clone(),
+    //         ..broom1
+    //     };
+
+    //     broom1.name.push_str(" I");
+    //     broom2.name.push_str(" II");
+
+    //     (broom1, broom2)
+    // }
+
+    // let hokey = Broom {
+    //     name: "Hokey".to_string(),
+    //     height: 60,
+    //     health: 100,
+    //     position: (100.0, 200.0, 0.0),
+    //     intent: BroomIntent::FetchWater,
+    // };
+
+    // let (hokey1, hokey2) = chop(hokey);
+
+    // assert_eq!(hokey1.name, "Hokey I");
+    // assert_eq!(hokey1.height, 30);
+    // assert_eq!(hokey1.health, 100);
+
+    // assert_eq!(hokey2.name, "Hokey II");
+    // assert_eq!(hokey2.height, 30);
+    // assert_eq!(hokey2.health, 100);
 
     // fn read_numbers(file: &mut dyn BufRead) -> Result<Vec<i64>, io::Error> {
     //     let mut numbers = vec![];
